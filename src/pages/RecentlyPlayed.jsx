e9463d5be18a7d397a794dd9e1397b37e8160f47
Andris <andrisbalogh444@gmail.com>
2024-08-06 00:23:08 +0200

    Update RecentlyPlayed.jsx

--- src/pages/RecentlyPlayed.jsx
+++ src/pages/RecentlyPlayed.jsx
@@ -13,8 +13,8 @@ function RecentlyPlayed() {
     const [previews, setPreviews] = useState([]);
     const [albumImages, setAlbumImages] = useState([]);
     const [currentTrackIndex, setCurrentTrackIndex] = useState(null);
-    const [blurredImage, setBlurredImage] = useState(null);
     const audioRef = useRef(null);
+    const [hoveredIndex, setHoveredIndex] = useState(-1);
 
     useEffect(() => {
         if (!token) {
@@ -59,15 +59,11 @@ function RecentlyPlayed() {
             if (currentTrackIndex === index) {
                 audioRef.current.pause();
                 setCurrentTrackIndex(null);
-                if (blurredImage != null){
-                    setBlurredImage(null);
-                }
                 
             } else {
                 audioRef.current.src = previewUrl;
                 audioRef.current.play();
                 setCurrentTrackIndex(index);
-                setBlurredImage(index);
             }
         }
     };
@@ -79,20 +75,22 @@ function RecentlyPlayed() {
                 <div className="recently-played-table my-10 w-4/5 shadow-2xl shadow-[#134611] text-[#134611]">
                     <div className="table-header rounded-xl scale-[1.01] bg-[#3DA35D] h-10 flex justify-around shadow-md shadow-[#134611]">
                         <div className="w-[5%] flex justify-center items-center min-w-14 ">Album</div>
-                        <div className="w-[40%] flex items-center px-2">Artist</div>
                         <div className="w-[40%] flex items-center px-2">Track</div>
+                        <div className="w-[40%] flex items-center px-2">Artist</div>
                         <div className="w-[25%] flex items-center px-2">Played At</div>
                     </div>
                     <div className="table-body pt-2 bg-[#96E072]">
                         {recentTracks.map((track, index) => (
-                            <div className="table-body-row cursor-pointer flex bg-[#96E072] rounded-xl duration-200 hover:shadow-xl hover:scale-105 hover:bg-[#3DA35D]" onClick={() => playPreview(previews[index], index)} >
+                            <div className="table-body-row cursor-pointer flex bg-[#96E072] rounded-xl duration-200 hover:shadow-xl hover:scale-105 hover:bg-[#3DA35D]" onClick={() => playPreview(previews[index], index)} onMouseEnter={() => setHoveredIndex(index)} 
+                            onMouseLeave={() => setHoveredIndex(-1)} >
                                 <div className="w-[5%] flex items-center justify-center min-w-14 py-1">
-                                    <img src={albumImages[index]} className={`w-12 rounded-xl shadow-sm shadow-[#134611] ${blurredImage === index ? "blur-[1px]" : ""} duration-200`} alt=""/>
-                                    <img src="../src/assets/play-button.svg" alt="" className={`opacity-0 z-10 drop-shadow-[0_0px_3px_rgba(0,0,0,1)] hover:${blurredImage === index ? "opacity-0" : "opacity-100"} w-5 absolute duration-200`} />
-                                    <img src="../src/assets/pause-button.svg" alt="" className={`opacity-0 z-0 drop-shadow-[0_0px_3px_rgba(0,0,0,1)] ${blurredImage === index ? "opacity-100" : ""} w-5 absolute duration-200`} />
+                                    <img src={albumImages[index]} className={`w-12 rounded-xl shadow-sm shadow-[#134611] ${currentTrackIndex === index ? "blur-[1px]" : ""} duration-200`} alt=""/>
+                                    <img src="../src/assets/play-button.svg" alt="" className={`absolute ${hoveredIndex === index && currentTrackIndex !== index ? "opacity-100 z-10" : "opacity-0 z-0"} drop-shadow-[0_0px_3px_rgba(0,0,0,1)] w-5 duration-200`} 
+ />
+                                    <img src="../src/assets/pause-button.svg" alt="" className={`opacity-0 z-0 drop-shadow-[0_0px_3px_rgba(0,0,0,1)] ${currentTrackIndex === index ? "opacity-100" : ""} w-5 absolute duration-200`} />
                                 </div>
-                                <div className="w-[40%] flex items-center px-2">{recentArtists[index]}</div>
                                 <div className="w-[40%] flex items-center px-2">{track}</div>
+                                <div className="w-[40%] flex items-center px-2">{recentArtists[index]}</div>
                                 <div className="w-[25%] flex items-center px-2">{formatDate(playedAt[index])}</div>
                             </div>
                         ))}
